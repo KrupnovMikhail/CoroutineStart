e3314708a6b5d9a2529c0f2dcb082aefa9fd78f4
Mikhail Krupnov <krupnovmn@gmail.com>
2024-01-04 15:30:48 +0300

    Structured Concurrency

--- app/src/main/java/com/krupnov/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/krupnov/coroutinestart/MainActivity.kt
@@ -7,6 +7,8 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.krupnov.coroutinestart.databinding.ActivityMainBinding
 import kotlinx.coroutines.Deferred
@@ -20,108 +22,13 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.buttonLoad.setOnClickListener {
-            binding.progress.isVisible = true
-            binding.buttonLoad.isEnabled = false
-            val deferredCity: Deferred<String> = lifecycleScope.async {
-                val city = loadCity()
-                binding.tvLocation.text = city
-                city
-            }
-            val deferredTemp: Deferred<Int> = lifecycleScope.async {
-                val temp = loadTemperature()
-                binding.tvTemperature.text = temp.toString()
-                temp
-            }
-            lifecycleScope.launch {
-                val city = deferredCity.await()
-                val temp = deferredTemp.await()
-                Toast.makeText(
-                    this@MainActivity,
-                    "City: $city Temp: $temp",
-                    Toast.LENGTH_SHORT
-                ).show()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-            }
-//            loadDataWithoutCoroutine()
-        }
-    }
-
-    private suspend fun loadData() {
-        Log.d("MMM MainActivity", "Load started: $this")
-        binding.progress.isVisible = true
-        binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-
-        binding.tvLocation.text = city
-        val temp = loadTemperature()
-
-        binding.tvTemperature.text = temp.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
-        Log.d("MMM MainActivity", "Load finished: $this")
-
-
-    }
-
-    private fun loadDataWithoutCoroutine(step: Int = 0, obj: Any? = null) {
-        when (step) {
-            0 -> {
-                Log.d("MMM MainActivity", "Load started: $this")
-                binding.progress.isVisible = true
-                binding.buttonLoad.isEnabled = false
-                loadCityWithoutCoroutine {
-                    loadDataWithoutCoroutine(1, it)
-                }
-            }
-            1 -> {
-                val city = obj as String
-                binding.tvLocation.text = city
-                loadTemperatureWithoutCoroutine(city) {
-                    loadDataWithoutCoroutine(2, it)
-                }
-            }
-            2 -> {
-                val temp = obj as Int
-                binding.tvTemperature.text = temp.toString()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-                Log.d("MMM MainActivity", "Load finished: $this")
-            }
-        }
-    }
-
-    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
-        Handler(Looper.getMainLooper()).postDelayed({
-            callback.invoke("Moscow")
-        }, 5000)
-    }
-
-    private suspend fun loadCity(): String {
-        delay(2000)
-        return "Moscow"
-
-    }
-
-    private fun loadTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            callback.invoke(17)
-        }, 5000)
-    }
-
-    private suspend fun loadTemperature(): Int {
-        delay(5000)
-        return 17
-
+        viewModel.method()
     }
 }
