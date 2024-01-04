8599eab341f4c26fe01be5bc1a33c7d0e80cc6cf
Mikhail Krupnov <krupnovmn@gmail.com>
2024-01-04 18:38:46 +0300

    Exception Handling

--- app/src/main/java/com/krupnov/coroutinestart/MainViewModel.kt
+++ app/src/main/java/com/krupnov/coroutinestart/MainViewModel.kt
@@ -3,12 +3,14 @@ package com.krupnov.coroutinestart
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
-import kotlin.concurrent.thread
 
 class MainViewModel: ViewModel() {
 
     private val parentJob = Job()
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.d(LOG_TAG, "Exception caught: $throwable")
+    }
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob + exceptionHandler)
 
     fun method() {
         val childJob1 = coroutineScope.launch {
@@ -17,12 +19,17 @@ class MainViewModel: ViewModel() {
         }
         val childJob2 = coroutineScope.launch {
             delay(2000)
-            childJob1.cancel()
             Log.d(LOG_TAG, "second coroutine finished")
-            Log.d(LOG_TAG, "Parent coroutine cancelled: ${parentJob.isCancelled}")
         }
-        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString())
-        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString())
+        val childJob3 = coroutineScope.launch {
+            delay(1000)
+            error()
+            Log.d(LOG_TAG, "third coroutine finished")
+        }
+    }
+
+    private fun error() {
+        throw RuntimeException()
     }
 
     override fun onCleared() {
